b2d2abdc124fdb50ab96a1d6dc4d1c87c3115fc8
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-09-13 17:00:48 -0300

    desafio 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -1,9 +1,17 @@
 const localStorageSimulator = require('../mocks/localStorageSimulator');
 const getSavedCartItems = require('../helpers/getSavedCartItems');
+const { expect } = require('@jest/globals');
 
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Testa a função getSavedCartItems no local storage', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toBeCalled();
+  });
+
+  test('testa a função getSavedCartItems com parâmetro no local storage', () => {
+    getSavedCartItems('cartItem');
+    expect(localStorage.getItem).toBeCalledWith('cartItem')
+  });
 });
